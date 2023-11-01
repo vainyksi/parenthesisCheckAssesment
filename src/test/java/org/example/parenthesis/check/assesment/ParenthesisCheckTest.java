690e201fe1b939f54b2eaedbae76736a20b2c55d
Michal Banik <banik.michal.sk@gmail.com>
2023-11-01 22:30:00 +0200

    history50

--- src/test/java/org/example/parenthesis/check/assesment/ParenthesisCheckTest.java
+++ src/test/java/org/example/parenthesis/check/assesment/ParenthesisCheckTest.java
@@ -17,7 +17,7 @@ public class ParenthesisCheckTest {
             Character character = str.charAt(i);
             if (isStartParenthesis(character)) {
                 parenthesisConsumed.push(character);
-            } else if (isEndParenthesisOfPreviouslyCOnsumed(character, parenthesisConsumed)) {
+            } else if (isEndParenthesisOfPreviouslyConsumed(character, parenthesisConsumed)) {
                 if (parenthesisConsumed.isEmpty()) {
                     return WRONG;
                 }
@@ -28,7 +28,7 @@ public class ParenthesisCheckTest {
         return parenthesisConsumed.isEmpty() ? OK : WRONG;
     }
 
-    private static boolean isEndParenthesisOfPreviouslyCOnsumed(Character character, Stack<Character> consumed) {
+    private static boolean isEndParenthesisOfPreviouslyConsumed(Character character, Stack<Character> consumed) {
         return List.of(')', ']', '}').contains(character);
     }
 
