7439f33a58addebcd713b839b6aa279a81b9b76c
Michal Banik <banik.michal.sk@gmail.com>
2023-11-01 21:29:00 +0200

    history2

--- src/test/java/org/example/parenthesis/check/assesment/ParenthesisCheck.java
+++ src/test/java/org/example/parenthesis/check/assesment/ParenthesisCheck.java
@@ -1,4 +1,19 @@
 package org.example.parenthesis.check.assesment;
 
+import org.junit.jupiter.api.Test;
+
 public class ParenthesisCheck {
+
+
+
+    @Test
+    void checkParenthesisOrderAndParity() {
+        String strPositive = "xxx(aaa)ahoj(tuky[tuk]){hoj}";
+        String strNegative = "xxx(zz(kk)hh]pp";
+    }
+
+    @Test
+    void () {
+
+    }
 }
