4448abd5b0a76531efe7eb37e40fe9ca711ab044
Michal Banik <banik.michal.sk@gmail.com>
2023-11-17 18:00:34 +0100

    refactor scripts

--- history-apply.sh
+++ history-apply.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
 
-if [[ $# -eq 0 ]] ; then
-    echo 'missing parameter'
-    exit 0
-fi
+# for i in {1..90} ; do [ $i != 4 ] && ./history-apply.sh $i; done
 
-NUM=$1
-echo 'applying patch '$NUM
 
-git cherry-pick --no-commit -X theirs ebcf073fd3fbdb288c0f0a1d17a605aa9b6b17f6 
-rm src/test/java/org/example/parenthesis/check/assesment/ParenthesisCheck.java
-rm src/test/java/org/example/parenthesis/check/assesment/parenthesisCheck.java
-git apply --whitespace=fix intellij_reverse_local_history/local_history$NUM.patch
-git add src/test/java/org/example/parenthesis/check/assesment/ParenthesisCheck.java 
-git add src/test/java/org/example/parenthesis/check/assesment/parenthesisCheck.java 
-git add src/test/java/org/example/parenthesis/check/assesment/ParenthesisCheckTest.java 
-git commit -m "history$NUM"
+remove_file_if_exist() {
+	FILE=$1
+	if [ -e $FILE ] ; then
+		rm $FILE;
+		echo $FILE "removed";
+	fi
+}
 
+apply_local_history_patch() {
+
+  if [[ $# -eq 0 ]] ; then
+      echo 'missing parameter'
+      exit 0
+  fi
+
+  NUM=$1
+  echo 'applying patch '$NUM
+
+  git cherry-pick --no-commit -X theirs ebcf073fd3fbdb288c0f0a1d17a605aa9b6b17f6
+  remove_file_if_exist src/test/java/org/example/parenthesis/check/assesment/ParenthesisCheck.java
+  remove_file_if_exist src/test/java/org/example/parenthesis/check/assesment/parenthesisCheck.java
+  git apply --whitespace=fix intellij_reverse_local_history/local_history$NUM.patch
+  git add .
+  git commit -m "history$NUM"
+}
+
+
+
+
+for i in {0..90} ; do [ $i != 4 ] && apply_local_history_patch $i; done
